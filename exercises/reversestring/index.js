0c678525fb27530ce2c4057fdae569ed4f50ed1d
jjeffrin <jjeffrin@gmail.com>
2023-06-27 19:03:12 +0530

    added solutions for reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((reversed, char) => {
+        return reversed = char + reversed
+    }, '')
+}
+
+// solution 1
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+// solution 2
+// function reverse(str) {
+//     let reversed = ''
+//     for (let char of str) {
+//         reversed = char + reversed
+//     }
+//     return reversed
+// }
 
 module.exports = reverse;
