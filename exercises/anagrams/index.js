849d3b76ffb7374015aa0840cf3830557355727c
jjeffrin <jjeffrin@gmail.com>
2023-06-28 17:23:25 +0530

    added solutions for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+
+// solution 1
+// function anagrams(stringA, stringB) {
+//     const charMapA = {}
+//     const charMapB = {}
+
+//     for (let char of stringA.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMapA[char] = charMapA[char] + 1 || 1
+//     }
+
+//     for (let char of stringB.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMapB[char] = charMapB[char] + 1 || 1
+//     }
+
+//     if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//         return false
+//     }
+
+//     for (let char in charMapA) {
+//         if (charMapA[char] !== charMapB[char]) {
+//             return false
+//         }
+//     }
+
+//     return true
+// }
+
+anagrams('Whoa! Hi!', 'Hi! Whoa!')
 
 module.exports = anagrams;
